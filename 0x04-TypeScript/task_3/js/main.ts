7c383b956fcd62407c237efcae4a748b5debdb8e
Valentine Irungu <irunguvalentine73@gmail.com>
2025-05-25 20:31:08 +0300

    feat: set up ambient type declarations and simulate database logic for RowElements

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,30 @@
+/// <reference path="./crud.d.ts" />
+
+import {RowID, RowElement} from './interface';
+import * as CRUD from './crud.js';
+
+const  row: RowElement = {
+    firstName: "Guillaume",
+    lastName: "Salva"
+};
+
+const newRowID: RowID = CRUD.insertRow(row);
+const updateRow: RowElement = {
+    ...row,
+    age: 23,
+};
+
+CRUD.updateRow(newRowID, updateRow);
+CRUD.deleteRow(newRowID);
+
+// Example usage of CRUD operations
+const obj = {firstName: "Guillaume", lastName: "Salva"};
+CRUD.insertRow(obj)
+// Insert row {firstName: "Guillaume", lastName: "Salva"}
+
+const updatedRow: RowElement = { firstName: "Guillaume", lastName: "Salva", age: 23 };
+CRUD.updateRow(newRowID, updatedRow);
+// Update row 125 {firstName: "Guillaume", lastName: "Salva", age: 23}
+
+CRUD.deleteRow(125);
+// Delete row id 125
